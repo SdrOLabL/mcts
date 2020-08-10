874da6d69bd1a2accc423c5b15a676854ffaa753
SdrOLabL <69472290+SdrOLabL@users.noreply.github.com>
2020-08-10 21:42:44 +0200

    Add files via upload

--- mcts.py
+++ mcts.py
@@ -6,29 +6,30 @@ import copy
 
 class Node:
     def __init__(self, state, action = None, parent = None):
-        self.wins = 0
+        self.wins = {1: 0, 2: 0}
         self.visit_count = 0
         self.state = state
         self.parent = parent
         # action -> the action that lead to this node
         self.action = action
         self.children = []
-    def get_value(self):
+    def get_value(self, player):
         if self.visit_count == 0: return math.inf
-        exploitation = self.wins / self.visit_count
+        exploitation = self.wins[player] / self.visit_count
         exploration = math.sqrt(2) * math.sqrt(math.log(self.parent.visit_count) / self.visit_count)
         # returns the uct value
         return exploitation + exploration
 
-def best_uct(node):
+def best_uct(node, player):
     # returns the child with the highest uct value
-    values = [n.get_value() for n in node.children]
+    values = [n.get_value(player) for n in node.children]
     return node.children[values.index(max(values))]
 
 def traverse(node, player):
+    player_copy = player
     # traverse the tree from the root node to a leaf node
     while node.children:
-        node = best_uct(node)
+        node = best_uct(node, player_copy)
         player = s.next_player(player)
     # check if node has a visit count and is not terminal
     if node.visit_count > 0 and (action_space := s.get_action_space(node.state)):
@@ -38,7 +39,7 @@ def traverse(node, player):
             state, _, _ = s.move(node.state, action, player)
             node.children.append(Node(state, action, node))
         node = node.children[0]
-    return node, player
+    return node, player, player_copy
 
 def rollout(node, player):
     state = copy.deepcopy(node.state)
@@ -48,36 +49,37 @@ def rollout(node, player):
         state, player, won = s.move(state, action, player)
     return won
 
-def best_child(node):
-    visit_counts = [n.visit_count for n in node.children]
-    return node.children[visit_counts.index(max(visit_counts))]
+def best_child(node, player):
+    wins = [n.wins[player] for n in node.children]
+    return node.children[wins.index(max(wins))]
 
-def backpropagate(node, simulation_result, player):
+def backpropagate(node, simulation_result, player, player_copy):
     while node:
         node.visit_count += 1
-        if player != simulation_result: node.wins += 1
+        if simulation_result in [1, 2]:
+            node.wins[simulation_result] += 1
         player = s.next_player(player)
         node = node.parent
 
 def mcts(root, player, iterations):
+    inital_player = player
     for _ in range(iterations):
-        leaf, player = traverse(root, player)
+        leaf, player, player_copy = traverse(root, player)
         simulation_result = rollout(leaf, player)
-        backpropagate(leaf, simulation_result, player)
-    return best_child(root)
+        backpropagate(leaf, simulation_result, player, player_copy)
+    return best_child(root, inital_player)
 
 root = Node(s.init())
 
 won = False
 # Player 1 = 1; Player 2 = 2; Nothing = 0
-player = random.randrange(3)
+player = random.randrange(2) + 1
 state = s.init()
 
 node = root
 
 while not won:
-    node = mcts(node, player, 1000)
-    # action = s.get_action_space_sample(state)
+    node = mcts(node, player, 10000)
     action = node.action
     state, player, won = s.move(state, action, player)
     print(state)
