81a9e7eecc533e4c479f66ba7f22b446fd78ad2d
SdrOLabL <69472290+SdrOLabL@users.noreply.github.com>
2020-08-12 12:36:37 +0200

    Add files via upload

--- state.py
+++ state.py
@@ -32,8 +32,8 @@ def check_won(state):
     diagonal1 = list(np.diag(np.fliplr(state)))
     diagonal2 = list(np.diag(state))
     if diagonal1.count(diagonal1[0]) == len(diagonal1) and diagonal1[0] != 0: return diagonal1[0]
-    if diagonal2.count(diagonal2[0]) == len(diagonal2) and diagonal1[0] != 0: return diagonal2[0]
+    if diagonal2.count(diagonal2[0]) == len(diagonal2) and diagonal2[0] != 0: return diagonal2[0]
     for row1, row2 in zip(state, state.T):
         if list(row1).count(row1[0]) == len(row1) and row1[0] != 0: return row1[0]
-        if list(row2).count(row1[0]) == len(row2) and row2[0] != 0: return row2[0]
+        if list(row2).count(row2[0]) == len(row2) and row2[0] != 0: return row2[0]
     if 0 not in state: return -1
